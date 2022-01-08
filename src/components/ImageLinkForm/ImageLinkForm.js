079021a213c3f025cfcc8b11651f4031fcf10d77
Jakov Knezevic <knezevic.jakov@gmail.com>
2022-01-08 19:23:22 +0100

    Fixed bugs.

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -17,7 +17,7 @@ const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
           />
           <button
             style={{
-              backgroundColor: "#3ca096",
+              backgroundColor: "#46afa6",
             }}
             className="w-30 grow f4 link ph3 pv2 dib white"
             onClick={onPictureSubmit}
